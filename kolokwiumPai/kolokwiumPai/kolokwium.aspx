f77b430cad973ab4dca699e8ba8899329217cf13
Uprzejmy <lmerynda@gmail.com>
2017-01-18 19:47:59 +0100

    init

---
+++ kolokwiumPai/kolokwiumPai/kolokwium.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="kolokwium.aspx.cs" Inherits="kolokwiumPai.kolokwium" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>Podaj x1:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>Podaj x2:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="policz" />
+        </p>
+        <p>test<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ kolokwiumPai/kolokwiumPai/kolokwium.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace kolokwiumPai
+{
+    public partial class kolokwium : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double x1 = double.Parse(TextBox1.Text);
+            double x2 = double.Parse(TextBox2.Text);
+
+            double result = integral(x1, x2);
+            Label1.Text = String.Format(" result: {0:0.0000}",result);
+        }
+
+        private double integral(double start, double end, double accuracy=0.001)
+        {
+            double result = 0.0;
+
+            for (double i = start; i < end; i += accuracy)
+            {
+                result += area(Math.Sin(i), Math.Sin(i + accuracy), accuracy);
+            }
+
+            return result;
+        }
+
+        private double integral(double start, double end, int intervals, double accuracy = 0.001)
+        {
+            double[] result = new double[intervals];
+            double interval = (end-start)/intervals;
+
+            Parallel.For(0, intervals, (int x)=> 
+            {
+                result[x] = 0.0;
+                for(double i = start;i<end;i+=accuracy)
+                {
+
+                }
+            }
+
+            for (double i = start; i < end; i += accuracy)
+            {
+                result += area(Math.Sin(i), Math.Sin(i + accuracy), accuracy);
+            }
+
+            return result;
+        }
+
+        private double area(double a, double b, double h)
+        {
+            return (a + b) * h / 2.0;
+        }
+    }
+}
